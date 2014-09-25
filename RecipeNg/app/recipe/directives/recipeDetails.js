2d7588939e6d5eda479fa3cdff5b730a1b880517
Tiberiu Covaci <tibi@covaci.se>
2014-09-25 16:02:57 +0200

    ng-Include recipeInstruction recipeDetails

---
+++ RecipeNg/app/recipe/directives/recipeDetails.js
@@ -0,0 +1,23 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('recipe')
+        .directive('recipeDetails', recipeDetails);
+
+    function recipeDetails () {
+        // Usage:
+        // 
+        // Creates:
+        // 
+        var directive = {
+            controller: 'recipeCtrl',
+            templateUrl: 'app/recipe/templates/details.html',
+            restrict: 'EA',
+            scope : {}
+        };
+        return directive;
+
+    }
+
+})();
---
+++ RecipeNg/app/recipe/directives/recipeInstructions.js
@@ -0,0 +1,28 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('recipe')
+        .directive('recipeInstructions', recipeInstructions);
+
+    function recipeInstructions () {
+        // Usage:
+        // 
+        // Creates:
+        // 
+        var directive = {
+            link: link,
+            restrict: 'EA',
+            templateUrl: 'app/recipe/templates/instructions.html',
+            scope : {
+                instructions : '=data'
+            }
+
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+        }
+    }
+
+})();
