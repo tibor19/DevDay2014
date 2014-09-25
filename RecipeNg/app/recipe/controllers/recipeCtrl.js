4e2246210d6c68d2c60af5fee715b19c915262c1
Tiberiu Covaci <tibi@covaci.se>
2014-09-25 15:30:05 +0200

    Rearranged the UI

--- RecipeNg/app/recipe/controllers/recipeCtrl.js
+++ RecipeNg/app/recipe/controllers/recipeCtrl.js
@@ -1,9 +1,6 @@
 ﻿(function () {
     angular.module('recipe')
-    .controller('recipeCtrl', ['$scope', 'resourceRecipeService', function ($scope, recipeService) {
-        $scope.recipe = {};
-        recipeService.getRecipe(1).then(function(data) {
-            $scope.recipe = data;
-        });
+    .controller('recipeCtrl', ['$scope', 'simpleRecipeService', function ($scope, recipeService) {
+        $scope.recipe = recipeService.getRecipe(1);
     }]);
 })(); 
